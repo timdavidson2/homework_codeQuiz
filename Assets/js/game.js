ef3878f996b6bc015e6dbab8a28069c7c5bcd748
timDavidson <timdavidson2@gmail.com>
2021-06-24 02:50:22 -0400

    correct JSON path to questions

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -2,13 +2,9 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
-const progressBarFull = document.getElementById('progress-bar-full');
-const loader = document.getElementById('loader');
-const game = document.getElementById('game');
-
-
-
-
+const progressBarFull = document.getElementById("progress-bar-full");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -18,22 +14,20 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch('questions.json')
-    .then(res => {
-        return res.json();
-    })
+fetch("../questions.json")
+  .then((res) => {
+    return res.json();
+  })
 
-    .then(loadedQuestions => {
-        console.log(loadedQuestions);
-        questions = loadedQuestions;
+  .then((loadedQuestions) => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
 
-        startGame();
-    })
-    .catch(err => {
-        alert("questions did not load, refresh to try again.")
-    });
-
-
+    startGame();
+  })
+  .catch((err) => {
+    alert("questions did not load, refresh to try again.");
+  });
 
 // **** CONSTANTS ****
 
@@ -41,90 +35,89 @@ const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 10;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-    game.classList.remove("hidden");
-    loader.classList.add("hidden");
-}
-
-getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCoutner >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-        // got to the end page
-        return window.location.assign("/end.html");
-    }
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    // update progress bar
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    })
-    availableQuestions.splice(questionIndex, 1);
-
-    acceptingAnswers = true;
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if (!acceptingAnswers) return;
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCoutner >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    // got to the end page
+    return window.location.assign("./end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  // update progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-        // const classToApply = 'incorrect';
-        // if (selectedAnswer == currrentQuestion.answer) {
-        //     classToApply = 'correct';
-        // }
-        // alternate possibility (teranary operator)
-        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-        selectedChoice.parentElement.classList.add(classToApply);
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+  availableQuestions.splice(questionIndex, 1);
 
-        if (classToApply === "correct") {
-            incrementScore(CORRECT_BONUS);
-        } else {
-            timeSecond -= 5;
-        }
+  acceptingAnswers = true;
+};
 
-        setTimeout(() => {
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
 
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-    });
+    // const classToApply = 'incorrect';
+    // if (selectedAnswer == currrentQuestion.answer) {
+    //     classToApply = 'correct';
+    // }
+    // alternate possibility (teranary operator)
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    } else {
+      timeSecond -= 5;
+    }
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
 
-incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
 };
 
 // **** Timer ****
-const timeH = document.getElementById('timer');
+const timeH = document.getElementById("timer");
 let timeSecond = MAX_QUESTIONS * 10;
 
-displayTime(timeSecond)
+displayTime(timeSecond);
 const countdown = setInterval(() => {
-    timeSecond--;
-    displayTime(timeSecond);
-    if (timeSecond <= 0 || timeSecond < 1) {
-        clearInterval(countdown)
-        return window.location.assign("/end.html");
-    }
-}, 1000)
+  timeSecond--;
+  displayTime(timeSecond);
+  if (timeSecond <= 0 || timeSecond < 1) {
+    clearInterval(countdown);
+    return window.location.assign("./end.html");
+  }
+}, 1000);
 
 function displayTime(second) {
-    const min = Math.floor(second / 60);
-    const sec = Math.floor(second % 60);
-    timeH.innerHTML = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  timeH.innerHTML = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
 }
